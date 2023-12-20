ae2f52cd621748abd2a7310d8657a8ce7b275075
Bei Li <6008854+li-bei@users.noreply.github.com>
2023-12-20 23:11:09 +0900

    Create UIViewPro

---
+++ Sources/UIKitPro/UIViewPro.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+open class UIViewPro: UIView {
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    // MARK: NSCoding
+    
+    @available(*, unavailable)
+    public required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
