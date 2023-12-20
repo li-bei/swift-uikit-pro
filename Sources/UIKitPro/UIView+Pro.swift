93a3ed1d657a8f0367fd53a6cc20b8af7c4d730b
Bei Li <6008854+li-bei@users.noreply.github.com>
2023-12-20 22:18:16 +0900

    Add UIView.systemBorderWidth

--- Sources/UIKitPro/UIView+Pro.swift
+++ Sources/UIKitPro/UIView+Pro.swift
@@ -2,4 +2,12 @@ import UIKit
 
 extension UIView {
     public var displayScale: CGFloat { traitCollection.displayScale }
+    
+    public var systemBorderWidth: CGFloat {
+        if traitCollection.preferredContentSizeCategory.isAccessibilityCategory {
+            1
+        } else {
+            1 / displayScale
+        }
+    }
 }
