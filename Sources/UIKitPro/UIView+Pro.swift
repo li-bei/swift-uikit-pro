e11680c4603a3cee1c21e77a0017f0f5addf6053
Bei Li <6008854+li-bei@users.noreply.github.com>
2023-12-20 23:21:46 +0900

    Add UIView.systemSpacing

--- Sources/UIKitPro/UIView+Pro.swift
+++ Sources/UIKitPro/UIView+Pro.swift
@@ -11,6 +11,8 @@ extension UIView {
         }
     }
     
+    public var systemSpacing: CGFloat { 8 }
+    
     public func heightThatFits(_ width: CGFloat = 0) -> CGFloat {
         return sizeThatFits(CGSize(width: width, height: 0)).height
     }
