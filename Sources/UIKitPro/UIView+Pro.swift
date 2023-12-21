9e297a48dc13d80d713e2f3110bc88defb01810d
Bei Li <6008854+li-bei@users.noreply.github.com>
2023-12-21 17:30:34 +0900

    Add UIView.sizeThatFits()

--- Sources/UIKitPro/UIView+Pro.swift
+++ Sources/UIKitPro/UIView+Pro.swift
@@ -16,4 +16,8 @@ extension UIView {
     public func heightThatFits(_ width: CGFloat = 0) -> CGFloat {
         return sizeThatFits(CGSize(width: width, height: 0)).height
     }
+    
+    public func sizeThatFits() -> CGSize {
+        return sizeThatFits(.zero)
+    }
 }
