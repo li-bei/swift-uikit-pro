65c7094e989f9b24e77002513c92f71ea774dd48
Bei Li <6008854+li-bei@users.noreply.github.com>
2023-12-22 14:10:34 +0900

    Add UIView.addSubviews(_:)

--- Sources/UIKitPro/UIView+Pro.swift
+++ Sources/UIKitPro/UIView+Pro.swift
@@ -13,6 +13,12 @@ extension UIView {
     
     public var systemSpacing: CGFloat { 8 }
     
+    public func addSubviews(_ views: [UIView]) {
+        for view in views {
+            addSubview(view)
+        }
+    }
+    
     public func heightThatFits(_ width: CGFloat = 0) -> CGFloat {
         return sizeThatFits(CGSize(width: width, height: 0)).height
     }
