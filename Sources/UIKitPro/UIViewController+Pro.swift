97a5f0c88038573b21ed09aedd6f7320150b4134
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-01-19 03:52:43 +0900

    Add UIViewController.localizedTitle

--- Sources/UIKitPro/UIViewController+Pro.swift
+++ Sources/UIKitPro/UIViewController+Pro.swift
@@ -2,4 +2,10 @@ import UIKit
 
 extension UIViewController {
     public var window: UIWindow? { viewIfLoaded?.window }
+    
+    @available(iOS 16.0, *)
+    public var localizedTitle: LocalizedStringResource {
+        get { fatalError() }
+        set { title = String(localized: newValue) }
+    }
 }
