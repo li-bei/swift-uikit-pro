60cf19e12f99737413bf3d32f134e273cd20127d
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-01-19 01:37:26 +0900

    Add UIListContentConfiguration.localizedText

--- Sources/UIKitPro/UIListContentConfiguration+Pro.swift
+++ Sources/UIKitPro/UIListContentConfiguration+Pro.swift
@@ -3,7 +3,8 @@ import UIKit
 @available(iOS 14.0, *)
 extension UIListContentConfiguration {
     @available(iOS 16.0, *)
-    public mutating func setLocalizedText(_ resource: LocalizedStringResource) {
-        text = String(localized: resource)
+    public var localizedText: LocalizedStringResource {
+        get { fatalError() }
+        set { text = String(localized: newValue) }
     }
 }
