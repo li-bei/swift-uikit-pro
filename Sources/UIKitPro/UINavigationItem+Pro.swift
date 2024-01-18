c24e1446e68aba995a9fc5d71640723531db81e3
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-01-19 01:37:10 +0900

    Add UINavigationItem.localizedTitle

--- Sources/UIKitPro/UINavigationItem+Pro.swift
+++ Sources/UIKitPro/UINavigationItem+Pro.swift
@@ -2,7 +2,8 @@ import UIKit
 
 extension UINavigationItem {
     @available(iOS 16.0, *)
-    public func setLocalizedTitle(_ resource: LocalizedStringResource) {
-        title = String(localized: resource)
+    public var localizedTitle: LocalizedStringResource {
+        get { fatalError() }
+        set { title = String(localized: newValue) }
     }
 }
