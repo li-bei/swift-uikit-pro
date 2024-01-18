a905b34fb576b7d25256f78ef56cfe1fa6eeb14d
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-01-18 17:08:52 +0900

    Update

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "UIKitPro",
+    name: "swift-uikit-pro",
     products: [
         .library(name: "UIKitPro", targets: ["UIKitPro"]),
     ],
