d797c5708c4b8122dff6faa935773f335b299d69
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-01-22 18:29:41 +0900

    Update

--- Sources/UIKitPro/UIBarButtonItem+Pro.swift
+++ Sources/UIKitPro/UIBarButtonItem+Pro.swift
@@ -3,11 +3,11 @@ import UIKit
 extension UIBarButtonItem {
     @available(iOS 16.0, *)
     public convenience init(
-        localizedTitle resource: LocalizedStringResource,
+        localizedTitle: LocalizedStringResource,
         image: UIImage? = nil,
         primaryAction: UIAction? = nil,
         menu: UIMenu? = nil
     ) {
-        self.init(title: String(localized: resource), image: image, primaryAction: primaryAction, menu: menu)
+        self.init(title: String(localized: localizedTitle), image: image, primaryAction: primaryAction, menu: menu)
     }
 }
