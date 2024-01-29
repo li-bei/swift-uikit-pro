5de04e8ebadacbab164f6c21af946020e1032538
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-01-29 23:17:35 +0900

    Update

--- Sources/UIKitPro/UINavigationItem+Pro.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-extension UINavigationItem {
-    @available(iOS 16.0, *)
-    public var localizedTitle: LocalizedStringResource {
-        get { fatalError() }
-        set { title = String(localized: newValue) }
-    }
-}
--- Sources/UIKitPro/UIViewController+Pro.swift
+++ Sources/UIKitPro/UIViewController+Pro.swift
@@ -2,10 +2,4 @@ import UIKit
 
 extension UIViewController {
     public var window: UIWindow? { viewIfLoaded?.window }
-    
-    @available(iOS 16.0, *)
-    public var localizedTitle: LocalizedStringResource {
-        get { fatalError() }
-        set { title = String(localized: newValue) }
-    }
 }
