84813cdf1470aafc18addf4c24f02bd2ad56c742
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-02-14 00:24:05 +0800

    Add NSLayoutXAxisAnchor.constraint(equalToSystemSpacingAfter:)

--- Sources/UIKitPro/NSLayoutXAxisAnchor+Pro.swift
+++ Sources/UIKitPro/NSLayoutXAxisAnchor+Pro.swift
@@ -1,6 +1,10 @@
 import UIKit
 
 extension NSLayoutXAxisAnchor {
+    public func constraint(equalToSystemSpacingAfter anchor: NSLayoutXAxisAnchor) -> NSLayoutConstraint {
+        return constraint(equalToSystemSpacingAfter: anchor, multiplier: 1)
+    }
+    
     public func constraint(
         equalToSystemSpacingBefore anchor: NSLayoutXAxisAnchor,
         multiplier: CGFloat = 1
