94f054ca53054fa2b75e857ea4ce82cfccf09658
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-02-18 17:48:39 +0900

    Add NSMutableAttributedString.append(_:)

--- Sources/UIKitPro/NSMutableAttributedString+Pro.swift
+++ Sources/UIKitPro/NSMutableAttributedString+Pro.swift
@@ -4,4 +4,8 @@ extension NSMutableAttributedString {
     public func addAttributes(_ attributes: [NSAttributedString.Key: Any]) {
         addAttributes(attributes, range: NSRange(location: 0, length: length))
     }
+    
+    public func append(_ string: String) {
+        append(NSAttributedString(string: string))
+    }
 }
