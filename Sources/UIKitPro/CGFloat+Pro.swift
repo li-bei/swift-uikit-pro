66c55e9625f0f573ce9ff28ed0aaef9d7d6a2697
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-02-26 17:03:28 +0900

    Add CGFloat.pixelAligned(_:)

--- Sources/UIKitPro/CGFloat+Pro.swift
+++ Sources/UIKitPro/CGFloat+Pro.swift
@@ -2,6 +2,10 @@ import CoreFoundation
 
 extension CGFloat {
     public mutating func pixelAlign(_ displayScale: CGFloat) {
-        self = (self * displayScale).rounded() / displayScale
+        self = self.pixelAligned(displayScale)
+    }
+    
+    public func pixelAligned(_ displayScale: CGFloat) -> CGFloat {
+        return (self * displayScale).rounded() / displayScale
     }
 }
