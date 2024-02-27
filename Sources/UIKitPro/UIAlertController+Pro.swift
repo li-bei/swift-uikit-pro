eb22fd65bde3f91e88213e54e8f5685efc6ad6a4
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-02-27 17:37:05 +0900

    Add UIAlertController.setActions(_:)

--- Sources/UIKitPro/UIAlertController+Pro.swift
+++ Sources/UIKitPro/UIAlertController+Pro.swift
@@ -4,4 +4,8 @@ extension UIAlertController {
     public convenience init(title: String) {
         self.init(title: title, message: nil, preferredStyle: .alert)
     }
+    
+    public func setActions(_ actions: [UIAlertAction]) {
+        setValue(actions, forKey: #keyPath(UIAlertController.actions))
+    }
 }
