fdab401e199c0b35c87a3d09dd7cd26d15ffccd5
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-03-10 17:53:35 +0900

    Update CGRect.init(x:y:size:)

--- Sources/UIKitPro/CGRect+Pro.swift
+++ Sources/UIKitPro/CGRect+Pro.swift
@@ -1,7 +1,7 @@
 import CoreFoundation
 
 extension CGRect {
-    public init(x: CGFloat, y: CGFloat, size: CGSize) {
+    public init(x: CGFloat = 0, y: CGFloat, size: CGSize) {
         self.init(origin: CGPoint(x: x, y: y), size: size)
     }
 }
