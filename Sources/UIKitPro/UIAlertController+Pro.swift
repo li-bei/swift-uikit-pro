648b61e3395cd0dec51ed2a1eaaaa3ca9d8c6cf9
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-03-13 01:56:47 +0900

    Update UIAlertController.init(title:message:)

--- Sources/UIKitPro/UIAlertController+Pro.swift
+++ Sources/UIKitPro/UIAlertController+Pro.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 extension UIAlertController {
-    public convenience init(title: String?, message: String?) {
+    public convenience init(title: String?, message: String? = nil) {
         self.init(title: title, message: message, preferredStyle: .alert)
     }
 
