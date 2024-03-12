d7f6768764b273a1364b4096271bf04eebba3119
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-03-12 22:17:53 +0900

    Create UIViewControllerPro

---
+++ Sources/UIKitPro/UIViewControllerPro.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+open class UIViewControllerPro: UIViewController {
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    // MARK: NSCoding
+
+    @available(*, unavailable)
+    public required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
