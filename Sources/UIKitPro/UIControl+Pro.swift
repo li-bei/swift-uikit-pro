95443fd8ce4c01374d8a610abb56b178c6f37dbb
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-03-19 19:26:33 +0900

    Add UIControl.init(primaryAction:)

---
+++ Sources/UIKitPro/UIControl+Pro.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+extension UIControl {
+    @available(iOS 14.0, *)
+    public convenience init(primaryAction: UIAction?) {
+        self.init(frame: .zero, primaryAction: primaryAction)
+    }
+}
