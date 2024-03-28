05558cafa7222220d6d8a72712b2b8a0668a2092
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-03-28 15:42:29 +0900

    Add UIView.systemSpacing

--- Sources/UIKitPro/UIView+Pro.swift
+++ Sources/UIKitPro/UIView+Pro.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 extension UIView {
+    public var systemSpacing: CGFloat { 8 }
+
     public func heightThatFits(_ width: CGFloat = 0) -> CGFloat {
         return sizeThatFits(CGSize(width: width, height: 0)).height
     }
