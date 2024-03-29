4c8fc734f503f25863b22da64b50f525f1395b49
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-03-29 18:33:16 +0900

    Add UITextView.attributedPlaceholder

---
+++ Sources/UIKitPro/UITextView+Pro.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension UITextView {
+    public var attributedPlaceholder: NSAttributedString? {
+        get {
+            value(forKey: #function) as? NSAttributedString
+        }
+        set {
+            setValue(newValue, forKey: #function)
+        }
+    }
+}
