60d1ceef3ee02808a454ad07c990ac806abb42e8
Bei Li <6008854+li-bei@users.noreply.github.com>
2024-04-04 15:25:22 +0900

    Add UILayoutPriority.almostDefaultLow

--- Sources/UIKitPro/UILayoutPriority+Pro.swift
+++ Sources/UIKitPro/UILayoutPriority+Pro.swift
@@ -2,4 +2,6 @@ import UIKit
 
 extension UILayoutPriority {
     public static var almostRequired: UILayoutPriority { .required - 1 }
+
+    public static var almostDefaultLow: UILayoutPriority { .defaultLow - 1 }
 }
